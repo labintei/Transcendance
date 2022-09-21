34c5c9bad7206684155ea2fcfecc2c68f662a00a
Marechal Baptiste <baptistemarechal42@gmail.com>
2022-09-21 10:55:03 +0200

    Unstable tries

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PersonList from './component/PersonList';
-
-function App() {
+//https://nextjs.org/docs/basic-features/layouts#with-typescript
+function App(Component: React.Component) {
   return (
     <div className="App">
-      <PersonList/>
+      <menu>
+        <li><button onclick={App({PersonList})}>Match</button></li>
+        <li><button onclick={App(PersonList)}>Friends</button></li>
+        <li><button onclick={App(PersonList)}>Profile</button></li>
+      </menu>
+      <div className="content">
+        <Component/>
+      </div>
     </div>
   );
 }
