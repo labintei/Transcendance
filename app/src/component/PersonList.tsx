57247e029e6485a26beba06f908a3c7dd07ae87f
bmarecha <baptistemarechal42@gmail.com>
2022-10-03 19:41:36 +0200

    Matching button

--- app/src/component/PersonList.tsx
+++ app/src/component/PersonList.tsx
@@ -8,32 +8,42 @@ type Person = {
     status: number;
     avatar_location: string;
     rank: number;
+    friend: boolean;
 }
 
 const defaultavatar = "https://cdn1.iconfinder.com/data/icons/ui-essential-17/32/UI_Essential_Outline_1_essential-app-ui-avatar-profile-user-account-512.png";
 
 type State = {
+  pwait:number
+  waiting:boolean
   listp:Array<Person>
+  listf:Array<Person>
 }
 
 export default class PersonList extends React.Component {
-  state:State= {listp:[]};
+  state:State= {pwait:0, listp:[], waiting:false, listf:[]};
 
   componentDidMount() {
     axios.get(`https://jsonplaceholder.typicode.com/users`)
       .then(res => {
         const persons = res.data;
-        let listtmp: Array<Person> = Array<Person>(0);   
+        let listftmp: Array<Person> = [];
+        let listtmp: Array<Person> = [];
         for (var person of persons) {
-            let one: Person = {id: 0, name: '', status: 0, avatar_location:defaultavatar, rank:1};
+            let one: Person = {id: 0, name: '', status: 0, avatar_location:defaultavatar, rank:1, friend:false};
             console.log(person);
             if (person.id !== undefined && person.name !== undefined) {
                 one.id = person.id;
                 one.name = person.name;
-                listtmp.push(one);
+                if (person.id < 4)
+                  one.friend = true;
+                if (one.friend)
+                  listftmp.push(one);
+                else
+                  listtmp.push(one);
             }
         }
-        this.setState({listp: listtmp});
+        this.setState({listp: listtmp, listf: listftmp, pwait: Math.trunc(Math.random() * 4)});
         console.log(this.state);
       })
   }
@@ -55,29 +65,78 @@ export default class PersonList extends React.Component {
       return ("In Match")
   }
 
-  get_friend_status(id:number) {
-    if (id > 4)
+  get_friend_status(isf:boolean) {
+    if (!isf)
       return ("add-f-button")
     else
       return ("remove-f-button")
   }
 
-  render() {
+  render_list(list:Array<Person>) {
     return (
-        <ul id="person-list">
+      <ul id="person-list">
         {
-          this.state.listp.map(person =>
-              <li key={person.id}>
-                <img alt="avatar" className="avatar" src={person.avatar_location}></img>
-                <p>{person.name}</p>
-                <p>{this.render_status(person.status)}</p>
-                <button onClick={() => this.challengeClicked(person.id)}  id="challenge-button"></button>
-                <img alt="friend" className="friend" src="https://cdn4.iconfinder.com/data/icons/basic-ui-2-line/32/people-group-team-peoples-friend-512.png"/>
-                <button onClick={() => this.friendManage(person.id)} id={this.get_friend_status(person.id)}></button>
-              </li>
-            )
+          list.map(person =>
+            <li key={person.id}>
+              <img alt="avatar" className="avatar" src={person.avatar_location}></img>
+              <p>{person.name}</p>
+              <p>{this.render_status(person.status)}</p>
+              <button onClick={() => this.challengeClicked(person.id)}  id="challenge-button"></button>
+              <img alt="friend" className="friend" src="https://cdn4.iconfinder.com/data/icons/basic-ui-2-line/32/people-group-team-peoples-friend-512.png"/>
+              <button onClick={() => this.friendManage(person.id)} id={this.get_friend_status(person.friend)}></button>
+            </li>
+          )
         }
       </ul>
+    )
+  }
+
+  render_friend(list:Array<Person>) {
+    if (list.length === 0)
+      return (<p>You've got no friends on the server for now !</p>)
+    else
+      return (
+        this.render_list(list)
+      )
+  }
+
+  render_others(list:Array<Person>) {
+    if (list.length === 0)
+      return (<p>There is no one else on the server for now !</p>)
+    else
+      return (
+        this.render_list(list)
+      )
+  }
+
+  change_waiting() {
+    if (this.state.waiting)
+    {
+      this.setState({waiting:false})
+      console.log("Start matching")
+    }
+    else
+    {
+      this.setState({waiting:true})
+      console.log("Stop Matching")
+    }
+
+  }
+
+  render() {
+    return (
+        <>
+        <h2>People waiting for a opponent : {this.state.pwait}</h2>
+        <button className="matching-button" onClick={() => this.change_waiting()}>{(this.state.waiting ? "Stop" : "Start") + " Matching"}</button>
+        <h3>Friends :</h3>
+        {
+          this.render_friend(this.state.listf)
+        }
+        <h3>Others :</h3>
+        {
+          this.render_others(this.state.listp)
+        }
+      </>
     )
   }
 }
