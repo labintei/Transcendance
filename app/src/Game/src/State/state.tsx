9f42847dd9dcba7148c99b6bf5e9dac7ee1c1afe
Eric Djavid <edjavid@student.42.fr>
2022-10-09 00:39:42 +0200

    dealing with collisions, relaunching countdown when ball lost, updating score

--- app/src/Game/src/State/state.tsx
+++ app/src/Game/src/State/state.tsx
@@ -8,7 +8,7 @@ export const useStore = create((set,get) => {
   return {
   set,
   get,
-  gameReady:true,
+  gameReady:false,
   votes: 0,
   score: [0,0],
   controls: {
@@ -19,9 +19,12 @@ export const useStore = create((set,get) => {
   box1: createRef(),
   box2: createRef(),
   addVotes: () => set((state:any) => ({ votes: state.votes + 1 })),
+  addPoint1: () => set((state:any) => ({ score: [state.score[0] + 1, state.score[1]]})),
+  addPoint2: () => set((state:any) => ({ score: [state.score[0], state.score[1] + 1]})),
   subtractVotes: () => set((state:any) => ({ votes: state.votes - 1 })),
   addBox: (box:any) => set((state:any) => ({ box1: [state.box[0] + box, state.box[1], state.box[2]] })),
   setReady: () => set((state:any) => ({ gameReady: true })),
+  setNotReady: () => set((state:any) => ({ gameReady: false })),
  }
 }
 
--- app/src/Game/src/World/Components/Sphere.tsx
+++ app/src/Game/src/World/Components/Sphere.tsx
@@ -7,52 +7,75 @@ import generateRandomFloatInRange from "./Skybox/Utils";
 // Geometry
 export default function Sphere() {
   const ref = useRef<any>(null!)
-  // @ts-ignore
-  const box1 = useStore((s) => s.box1)
-  // @ts-ignore
-  const box2 = useStore((s) => s.box2)
+  const box1:any = useStore((s:any) => s.box1)
+  const box2:any = useStore((s:any) => s.box2)
+  const gameNotReady:any = useStore((state:any) => state.setNotReady);
 
   const ready = useStore((s:any) => s.gameReady)
+  const addPoint1:any = useStore((state:any) => state.addPoint1);
+  const addPoint2:any = useStore((state:any) => state.addPoint2);
   
-  var direction = 0.1;
-  var angle = 0
+  var zdirection = 0.05;
+  var l = Math.random();
+  console.log(l)
+  if (l < 0.5)
+    zdirection = -0.05;
+  console.log(zdirection)
+  var xangle = l *0.1;
   // delta = temps de chargement d'une frame par l'OS
   useFrame((state, delta) => { 
+    // todo: randomize angles and increase fast when touched
 
     if (!ready)
       return;
     const width = box1.current.geometry.parameters.width;
     var sphereZ = Math.floor( ref.current.position.z );
     var sphereX = Math.round(ref.current.position.x * 10) / 100;
-    var box2Pos = box2.current.position
+    var box2X = Math.round(box2.current.position.x * 10) / 100;
+    var box1X = Math.round(box1.current.position.x * 10) / 100;
+    var good_width = Math.round((width / 2) * 10) / 100;
      
-    ref.current.position.z += direction;
-    ref.current.position.x += angle
+    ref.current.position.z += zdirection;
+    ref.current.position.x += xangle
     var sphereXInt = Math.round(ref.current.position.x)
     
-    console.log(sphereXInt)
-    // if collision with a box
-    if (sphereZ === box1.current.position.z && 
-      ((sphereX >= box1.current.position.x - (width / 2)) && 
-      sphereX <= box1.current.position.x + (width / 2)))
+    //! Collision with a box
+    // console.log(sphereZ)
+    // console.log(sphereX)
+    // console.log(box1.current.position.z)
+    // console.log(box1X)
+    if (sphereZ === box1.current.position.z &&
+      ((sphereX >= (box1X - good_width)) && 
+      (sphereX <= (box1X + good_width))))
     {
-      direction = -0.1;
-      // TODO: randomize
-      angle = generateRandomFloatInRange(0, 0.05)
+      zdirection = -0.1;
     }
-    if (sphereZ === box2.current.position.z &&
-      ((sphereX >= box2.current.position.x - (width / 2)) && 
-      sphereX <= box2.current.position.x + (width / 2)))
+    if (sphereZ == box2.current.position.z &&
+      ((sphereX >= (box2X - good_width)) && 
+      (sphereX <= (box2X + good_width))))
     {
-      direction = +0.05;
-      angle = generateRandomFloatInRange(0, 0.05)
+      zdirection = +0.1;
+      // xangle = generateRandomFloatInRange(0, 0.05)
     }
-    // Collision with border
+    //! Collision with border
     if ( sphereXInt === -5 || sphereXInt === 5)
     {
-      // angle = 1 -angle;
-      // direction = direction *-1;
+      console.log("collision!")
+      // todo: animation uppon collision
+      xangle = -xangle;
     } 
+
+    if (sphereZ > 7 || sphereZ < -7)
+    {
+      console.log("ball lost!!")
+      ref.current.position.z = 0;
+      ref.current.position.x = 0;
+      if (sphereZ > 7)
+      addPoint1()
+      else
+      addPoint2()
+      gameNotReady();  
+    }
     })
   return (
     <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow
