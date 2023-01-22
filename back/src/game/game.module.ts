c41bc1d0075f3e933943a68ef0294f0b94791d10
Lauranne BINTEIN <labintei@e1r9p6.clusters.42paris.fr>
2023-01-22 19:08:24 +0100

    resolution conflit

--- back/src/game/game.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GameController } from './game.controller';
-import { GameGateway } from './game.gateway';
-
-@Module({
-  controllers: [GameController],
-  providers: [GameGateway]
-})
-export class GameModule {}
