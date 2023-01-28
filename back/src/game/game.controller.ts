10c3c4d314824bdaab0fa169d9aa1c732d55beee
loutre <loutre@courrier.dev>
2023-01-28 15:08:12 +0100

    feat(back): invite to game with a message

--- back/src/game/game.controller.ts
+++ back/src/game/game.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param, Request, UseGuards } from '@nestjs/common';
 import { GameService } from './game.service'
 import { TransGuard } from 'src/auth/trans.guard';
 import { Match } from 'src/entities/match.entity';
+
+import { SocketGateway } from 'src/socket/socket.gateway';
+import { Channel } from 'src/entities/channel.entity';
+import { User } from 'src/entities/user.entity';
+import { Message } from 'src/entities/message.entity';
 
 @Controller('match')
 @UseGuards(TransGuard)
@@ -10,9 +15,33 @@ export class GameController {
 
   @Get(':username')
   async invitation(@Request() req , @Param('username') user: string): Promise<number> {
-    var c = this.gameService.CreateInvit(req.user, user); // normalement fonctionne 
-    var urlredir = process.env.REACT_APP_FRONTEND_URL + '/game/' + await c;
-    return c;
+    try {
+      const user1 = await User.findOneBy({ft_login: req.user});
+      const user2 = await User.findOneBy({username: user});
+      if (!user1)
+        throw new HttpException("User invalid", HttpStatus.BAD_REQUEST);
+      if (!user2)
+        throw new HttpException("Invited user invalid", HttpStatus.BAD_REQUEST);
+      var c = this.gameService.CreateInvit(user1, user2); // normalement fonctionne 
+      var urlredir: string = process.env.REACT_APP_WEBSITE_URL + 'game/' + await c;
+      const channelId = await Channel.getDirectChannelId(user1.ft_login, user2.ft_login);
+      const message = await Message.create({
+        content: urlredir,
+        senderLogin: user1.ft_login,
+        channelId: channelId
+      }).save();
+      message.sender = await User.findOne({
+        select: User.defaultFilter,
+        where: {
+          ft_login: user1.ft_login
+        }
+      });
+      SocketGateway.channelEmit(message.channelId, 'message', message);
+      return c;
+    }
+    catch (e) {
+      console.log("[Error Invite]", e);
+    }
   }
 
   @Get('history')
