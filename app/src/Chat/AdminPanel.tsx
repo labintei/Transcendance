ac6e9f0dbbcd67ded3d3a59f393c0fc5704e42e0
loutre <loutre@courrier.dev>
2023-01-28 19:10:10 +0100

    feat: add kick and ban, ban only works for 10s

--- app/src/Chat/AdminPanel.tsx
+++ app/src/Chat/AdminPanel.tsx
@@ -7,9 +7,9 @@ import {
 import { getLoginContext } from 'WebSocketWrapper';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faKey, faLock, faLockOpen, faPen, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBan, faCommentSlash, faUserSlash } from '@fortawesome/free-solid-svg-icons';
 
-import { tUser, tChannel } from './ChatPage';
+import { tUser, tChannelUser, tChannel } from './ChatPage';
 import { Socket } from 'socket.io-client';
 
 interface AdminProps {
@@ -20,11 +20,30 @@ interface AdminProps {
 export default function AdminPanel(props: AdminProps) {
     const login = useContext(getLoginContext);
 
-    function isAdmin(users: any) : boolean {
+    let timestamp: HTMLInputElement | null = null;
+
+    function isAdmin(users: tChannelUser[]) : boolean {
       console.log("isAdmin", users);
       const user = users.find((element: any) => element.user.ft_login === login.value);
 
+      if (user === undefined)
+        return false;
       return user.rights === "Owner" || user.rights === "Admin"
+    }
+
+    const setPermissionsUser = (user: tChannelUser, type: string) => (e: any) => {
+      const updated_user = user;
+      const time = new Date();
+
+      if (type === "Kicked")
+        updated_user.rights = null;
+      if (type === "Banned" || type === "Muted") {
+        time.setSeconds(time.getSeconds() + 10);
+        updated_user.rights = type;
+        updated_user.rightsEnd = time;
+      }
+
+      props.socket.emit('setPermissions', updated_user);
     }
 
     if (!isAdmin(props.currentChannel.users))
@@ -33,12 +52,35 @@ export default function AdminPanel(props: AdminProps) {
     return (
       <>
         <h1>Admin Admin !</h1>
-        {props.currentChannel.users.map((data: any, index) => {
+        {props.currentChannel.users.map((data, index) => {
           const user : tUser = data.user;
           return (
             <div key={index}>
-              <Avatar src={user.avatarURL} name={user.username} size="md"/>
-              
+                <p>{user.username}</p>
+                <Button
+                  icon={<FontAwesomeIcon icon={faCommentSlash} />}
+                  title="Mute user"
+                  >
+                    Mute
+                </Button>
+                <Button
+                  icon={<FontAwesomeIcon icon={faUserSlash} />}
+                  title="Kick user"
+                  onClick={setPermissionsUser(data, "Kicked")}
+                  >
+                    Kick
+                </Button>
+                <Button
+                  icon={<FontAwesomeIcon icon={faBan} />}
+                  title="Ban user"
+                  onClick={setPermissionsUser(data, "Banned")}
+                  >
+                    Ban
+                </Button>
+                <input
+                  type="number"
+                  placeholder="Fouts des minutes tocards"
+                />
             </div>
           )
         })}
